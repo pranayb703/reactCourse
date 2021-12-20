8985647cdc35c889debb7467261d702c15111cea
Pranay Bhandarkar <pranayb.27@gmail.com>
2021-12-19 22:09:45 -0800

    React course first commit

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,24 @@
+import './ExpenseItem.css';
+const ExpenseItem = () => {
+    const expenseDate = new Date(2021, 11, 12);
+    const expenseTitle = "Car insurance";
+    const expenseAmount = 300;
+    const month = expenseDate.toLocaleString('en-US', { month: 'long' });
+    const date = expenseDate.getUTCDate();
+    const year = expenseDate.getFullYear();
+    console.log("Date : " + date);
+    return (
+        <div className='expense-item'>
+            <div>
+                <div >{month}</div>
+                <div>{year}</div>
+                <div>{date}</div>
+            </div>
+            <div className='expense-item__description '>
+                <h2>{expenseTitle}</h2>
+                <div className='expense-item__price'>{expenseAmount}</div></div>
+        </div>
+    )
+}
+
+export default ExpenseItem
